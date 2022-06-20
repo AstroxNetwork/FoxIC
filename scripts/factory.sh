6ddddd9399a168840fa87f21243ac5a6d503bd34
neeboo <ghostcorn@gmail.com>
2022-06-20 10:43:13 +0800

    :fire: remove sh files

--- scripts/factory.sh
+++
@@ -1,2 +0,0 @@
-cd canisters/foxic_factory && 
-dfx canister --network=ic call foxic_factory add_owner '(principal "wzmn5-x5ep5-jbbjy-uexkb-aj7gf-kq2mo-ujdws-iizyz-qbdq3-wywpm-4qe")'
--- scripts/recycle.sh
+++
@@ -1,2 +0,0 @@
-cd canisters/foxic_factory && dfx canister --network=ic uninstall-code foxic_factory
-cd clients/front_end && dfx canister --network=ic uninstall-code foxic_frontend
