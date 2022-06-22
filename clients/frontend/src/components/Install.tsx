f5f32a0128140732edc3d88e2cf3366ef0e21b78
kasumisk <kasumisk@163.com>
2022-06-22 17:24:04 +0800

    added how to connect..

--- clients/frontend/src/components/Install.tsx
+++ clients/frontend/src/components/Install.tsx
@@ -1,10 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import LOGO from "../assets/logo.png"
 import FOX from "../assets/fox.png"
 import FOX_ANIMATE from "../assets/FOXIC.json"
 import Lottie from "react-lottie"
 import { METAMASK_FLASK } from "../config"
-import { ICON_METAMASK_FLASH, METAMASK } from "../utils/resCont"
+import {
+  ICON_CLOSE,
+  ICON_METAMASK_FLASH,
+  ICON_OPEN,
+  METAMASK,
+} from "../utils/resCont"
 
 const defaultOptions = {
   loop: true,
@@ -15,7 +20,96 @@ const defaultOptions = {
   },
 }
 
+type InstallModalProps = {
+  onClose: () => void
+}
+
+const InstallModal = (props) => {
+  return (
+    <div className="install-modal">
+      <h1 className="c_grey" style={{ fontSize: 30 }}>
+        1.
+      </h1>
+      <h2 className="mg_b_30">
+        Install{" "}
+        <a
+          href={METAMASK_FLASK}
+          target="_blank"
+          className="c_brand"
+          style={{ textDecoration: "underline" }}
+        >
+          MetaMask Flask
+        </a>
+      </h2>
+      <h1 className="c_grey" style={{ fontSize: 30 }}>
+        2.
+      </h1>
+      <h2>Create a new account</h2>
+      <p className="c_grey mg_b_30">
+        Follow the instructions in MetaMask Flask to create a new account and
+        save your recovery phrase{" "}
+        <strong className="c_brand">(IMPORTANT!)</strong>.
+      </p>
+      <h1 className="c_grey" style={{ fontSize: 30 }}>
+        3.
+      </h1>
+      <h2>Plugin settings</h2>
+      <div className="flex mg_b_30">
+        <div className="flex-1">
+          <div className="flex align-items-end">
+            <div>
+              <img src={METAMASK} alt="" style={{ width: 32, height: 30 }} />
+              <p>Turn off your</p>
+            </div>
+            <div>
+              <img src={ICON_CLOSE} alt="" style={{ width: 24, height: 14 }} />
+              <p>
+                <span style={{ color: "orange" }}>&nbsp;MetaMask</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="flex align-items-end">
+            <div>
+              <img
+                src={ICON_METAMASK_FLASH}
+                alt=""
+                style={{ width: 32, height: 30 }}
+              />
+              <p>Turn off your </p>
+            </div>
+            <div>
+              <p style={{ textAlign: "left" }}>
+                <img src={ICON_OPEN} alt="" style={{ width: 24, height: 14 }} />
+              </p>
+              <p>
+                <span style={{ color: "#A167FF" }}>&nbsp;MetaMask FLASH</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="c_grey" style={{ fontSize: 30 }}>
+        4.
+      </h1>
+      <h2>Connect MetaMask Flask</h2>
+      <p className="c_grey">
+        Refresh the website and click the "Connect" button.
+      </p>
+      <a
+        className={`button-primary button-block`}
+        style={{ marginTop: 50 }}
+        onClick={props.onClose}
+      >
+        Close
+      </a>
+    </div>
+  )
+}
+
 const Install = (props) => {
+  const [visible, setVisible] = useState(true)
   return (
     <div className="container">
       <div className="header">
@@ -62,6 +156,18 @@ const Install = (props) => {
           >
             Connect to FoxIC
           </a>
+          <a
+            className="button-default"
+            style={{ marginTop: 25, width: 198 }}
+            onClick={() => setVisible(true)}
+          >
+            How to connect ?
+          </a>
+        </div>
+      </div>
+      <div className={`modal ${visible ? "show" : ""}`}>
+        <div className={`modal-content modal-big`}>
+          <InstallModal onClose={() => setVisible(false)} />
         </div>
       </div>
     </div>
--- clients/frontend/src/utils/resCont.ts
+++ clients/frontend/src/utils/resCont.ts
@@ -8,5 +8,7 @@ import DELETE from "../assets/delete.png"
 import TIPS from "../assets/tips.png"
 import METAMASK from "../assets/icon-metamask.png"
 import ICON_METAMASK_FLASH from "../assets/icon-metamask-fox.svg"
+import ICON_OPEN from "../assets/icon-open.svg"
+import ICON_CLOSE from "../assets/icon-close.svg"
 
-export { FOX, LOGO, COPY, CODE, REFRESH, UPDATE, DELETE, TIPS, METAMASK, ICON_METAMASK_FLASH }
+export { FOX, LOGO, COPY, CODE, REFRESH, UPDATE, DELETE, TIPS, METAMASK, ICON_METAMASK_FLASH, ICON_CLOSE, ICON_OPEN }
